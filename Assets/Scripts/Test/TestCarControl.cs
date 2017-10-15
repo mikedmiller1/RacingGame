de1a792655b219b46bf9e15faf27aae5d699f999
Matt Weber <m@ttweber.com>
2017-10-15 18:39:29 -0400

    Control changes

--- Assets/Scripts/Test/TestCarControl.cs
+++ Assets/Scripts/Test/TestCarControl.cs
@@ -61,53 +61,81 @@ public class TestCarControl : MonoBehaviour
 
     float GetAccelerationConstant(int index)
     {
-        var baseAcceleration = 0.005f; // m/s/s
-        switch (index) {
-            case 0: return baseAcceleration;
-            case 1: return baseAcceleration * 1.5f;
-            case 2: return baseAcceleration * 2.25f;
-            case 3: return baseAcceleration * 3.375f;
-            case 4: return baseAcceleration * 5.0625f;
-            default: return baseAcceleration * 7.59375f;
+        var baseAcceleration = 0.5f; // m/s/s
+        switch (index)
+        {
+        case 0:
+            return baseAcceleration;
+        case 1:
+            return baseAcceleration * 1.5f;
+        case 2:
+            return baseAcceleration * 2.25f;
+        case 3:
+            return baseAcceleration * 3.375f;
+        case 4:
+            return baseAcceleration * 5.0625f;
+        default:
+            return baseAcceleration * 7.59375f;
         }
     }
 
-    float GetBrakingConstant(int index)
+    float GetBrakingMultiplier(int index)
     {
-        var baseBraking = 0.015f; // m/s/s
-        switch (index) {
-            case 0: return baseBraking;
-            case 1: return baseBraking * 1.5f;
-            case 2: return baseBraking * 2.25f;
-            case 3: return baseBraking * 3.375f;
-            case 4: return baseBraking * 5.0625f;
-            default: return baseBraking * 7.59375f;
+        var baseBraking = 1; // m/s/s
+        switch (index)
+        {
+        case 0:
+            return baseBraking;
+        case 1:
+            return baseBraking * 1.5f;
+        case 2:
+            return baseBraking * 2.25f;
+        case 3:
+            return baseBraking * 3.375f;
+        case 4:
+            return baseBraking * 5.0625f;
+        default:
+            return baseBraking * 7.59375f;
         }
     }
 
     float GetMaximumVelocity(int index)
     {
-        var baseMaximumVelocity = 0.05f; // m/s
-        switch (index) {
-            case 0: return baseMaximumVelocity;
-            case 1: return baseMaximumVelocity * 1.5f;
-            case 2: return baseMaximumVelocity * 2.25f;
-            case 3: return baseMaximumVelocity * 3.375f;
-            case 4: return baseMaximumVelocity * 5.0625f;
-            default: return baseMaximumVelocity * 7.59375f;
+        var baseMaximumVelocity = 0.01f; // m/s
+        switch (index)
+        {
+        case 0:
+            return baseMaximumVelocity;
+        case 1:
+            return baseMaximumVelocity * 1.5f;
+        case 2:
+            return baseMaximumVelocity * 2.25f;
+        case 3:
+            return baseMaximumVelocity * 3.375f;
+        case 4:
+            return baseMaximumVelocity * 5.0625f;
+        default:
+            return baseMaximumVelocity * 7.59375f;
         }
     }
 
     float GetHandlingConstant(int index)
     {
         var baseHandling = 12f; // degrees / s
-        switch (index) {
-            case 0: return baseHandling;
-            case 1: return baseHandling * 1.5f;
-            case 2: return baseHandling * 2.25f;
-            case 3: return baseHandling * 3.375f;
-            case 4: return baseHandling * 5.0625f;
-            default: return baseHandling * 7.59375f;
+        switch (index)
+        {
+        case 0:
+            return baseHandling;
+        case 1:
+            return baseHandling * 1.5f;
+        case 2:
+            return baseHandling * 2.25f;
+        case 3:
+            return baseHandling * 3.375f;
+        case 4:
+            return baseHandling * 5.0625f;
+        default:
+            return baseHandling * 7.59375f;
         }
     }
 
@@ -116,16 +144,40 @@ public class TestCarControl : MonoBehaviour
         var acceleratorInput = Input.GetAxis("Vertical");
         var steeringInput = Input.GetAxis("Horizontal");
 
-        acceleration = transform.up;
-
-        acceleration *= GetAccelerationConstant(AccelerationIndex) * acceleratorInput;
+        //acceleration = transform.up * GetAccelerationConstant(AccelerationIndex) * acceleratorInput;
+        acceleration = transform.up * (acceleratorInput > 0 ? acceleratorInput : coast);
 
         if (!Mathf.Approximately(steeringInput, 0))
         {
             acceleration = Quaternion.Euler(0, 0, -Mathf.Sign(steeringInput) * GetHandlingConstant(HandlingIndex)) * acceleration;
         }
 
-        velocity = Vector3.MoveTowards(transform.position, transform.position + acceleration, Time.fixedDeltaTime) - transform.position;
+        //velocity = Vector3.MoveTowards(transform.position, transform.position + acceleration, Time.fixedDeltaTime) - transform.position;
+        //velocity = acceleration.normalized * Time.fixedDeltaTime;
+        if (acceleratorInput > 0)
+        {
+            coast = 1;
+            velocity = Vector3.Lerp(transform.up, acceleration.normalized, GetAccelerationConstant(AccelerationIndex));
+            velocity = Vector3.ClampMagnitude(velocity, GetMaximumVelocity(MaximumVelocityIndex));
+        }
+        else if (coast > 0)
+        {
+            var slowFactor = 0.01f;
+            if (acceleratorInput < 0)
+            {
+                slowFactor *= GetBrakingMultiplier(BrakingIndex);
+            }
+
+            coast -= slowFactor * Time.fixedDeltaTime;
+            coast = Mathf.Clamp01(coast);
+
+            velocity = Vector3.Lerp(Vector3.zero, velocity, coast);
+            if (velocity.magnitude < 1e-4f)
+            {
+                velocity = Vector3.zero;
+                coast = 0;
+            }
+        }
 
         uiAccelerator.value = Mathf.Clamp01(acceleratorInput);
         uiBrake.value = Mathf.Abs(Mathf.Clamp(acceleratorInput, -1, 0));
@@ -163,12 +215,15 @@ public class TestCarControl : MonoBehaviour
     void Update()
     {
         UpdateAttributes();
-        Vector3.ClampMagnitude(velocity, GetMaximumVelocity(MaximumVelocityIndex));
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            transform.position = Vector3.zero;
+        }
 
         Debug.DrawRay(transform.position, acceleration * 1000f, Color.cyan);
         Debug.DrawRay(transform.position, velocity * 100f, Color.magenta);
         Debug.DrawRay(transform.position, transform.up * 10f, Color.yellow);
-
 
         var degreesToRotate = 0f;
         if (velocity.magnitude > 0)
@@ -199,6 +254,8 @@ public class TestCarControl : MonoBehaviour
 
     private Vector3 acceleration;
     private Vector3 velocity;
+
+    private float coast;
 
     [SerializeField]
     private int MaximumVelocityIndex;
