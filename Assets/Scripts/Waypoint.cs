489944fb2b7d38487cab05e7baf4a560b40ba4d3
Matt Weber <m@ttweber.com>
2017-10-17 23:07:06 -0400

    Added a circuit

---
+++ Assets/Scripts/Waypoint.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+    // Use this for initialization
+    void Start ()
+    {
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawIcon(GetPosition(), "Waypoint.png", false);
+    }
+
+    public Vector3 GetPosition()
+    {
+        return transform.position + Offset;
+    }
+
+    public Vector3 Offset;
+}
---
+++ Assets/Track.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Track : MonoBehaviour
+{
+    // Use this for initialization
+    void Start ()
+    {
+        var waypoints = GetComponentsInChildren<Waypoint>();
+        foreach (var waypoint in waypoints)
+        {
+            print(waypoint.GetPosition());
+        }
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+
+    }
+}
