86e55fca09a7c57487e9b97575141225065a8286
Mike Miller <mike.d.miller1@gmail.com>
2017-10-28 12:59:21 -0400

    Added an option to rotate the main camera with the player car.

--- Assets/Scripts/Test/TestCamera.cs
+++ Assets/Scripts/Test/TestCamera.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class TestCamera : MonoBehaviour
 {
+    /// <summary>
+    /// Flag to control rotating the camera view as the player car rotates around the track
+    /// </summary>
+    public bool RotateWithPlayer;
+
+
     // Use this for initialization
     void Start()
     {
@@ -14,6 +20,11 @@ public class TestCamera : MonoBehaviour
     void Update()
     {
         transform.position = Target.transform.position;
+
+        if( RotateWithPlayer )
+        {
+            transform.rotation = Target.transform.rotation;
+        }
     }
 
     [SerializeField]
