b050fdf558d46e384e8db33682c0681606d1fa5b
Mike Miller <mike.d.miller1@gmail.com>
2017-11-30 23:09:40 -0500

    Fixed grid positions

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,77 +5,77 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
 
-	#region Properties
+    #region Properties
 
-	/// <summary>
-	/// The number of AI drivers.
-	/// </summary>
-	public float NumberOfAIDrivers;
-
-
-
-	/// <summary>
-	/// List of AI driver objects.
-	/// </summary>
-	[HideInInspector]
-	public List<GameObject> AIDrivers;
+    /// <summary>
+    /// The number of AI drivers.
+    /// </summary>
+    public float NumberOfAIDrivers;
 
 
 
-	/// <summary>
-	/// Reference to the track object.
-	/// </summary>
-	public GameObject Track;
+    /// <summary>
+    /// List of AI driver objects.
+    /// </summary>
+    [HideInInspector]
+    public List<GameObject> AIDrivers;
 
 
 
-	/// <summary>
-	/// Reference to the opponent prefab.
-	/// </summary>
-	public GameObject PlayerPrefab;
+    /// <summary>
+    /// Reference to the track object.
+    /// </summary>
+    public GameObject Track;
 
 
 
-	/// <summary>
-	/// List of waypoint locations around the track.
-	/// </summary>
-	[HideInInspector]
-	public List<Vector2> Waypoints = new List<Vector2> ();
+    /// <summary>
+    /// Reference to the opponent prefab.
+    /// </summary>
+    public GameObject PlayerPrefab;
 
 
 
-	/// <summary>
-	/// The Waypoint game object to use when debugging.
-	/// </summary>
-	public GameObject WaypointPrefab;
+    /// <summary>
+    /// List of waypoint locations around the track.
+    /// </summary>
+    [HideInInspector]
+    public List<Vector2> Waypoints = new List<Vector2> ();
 
 
 
-	/// <summary>
-	/// Radius within which the player has arrived at the waypoint.
-	/// </summary>
-	public float WaypointArriveRadius;
+    /// <summary>
+    /// The Waypoint game object to use when debugging.
+    /// </summary>
+    public GameObject WaypointPrefab;
 
 
 
-	/// <summary>
-	/// The initial spacing of the drivers for the start.
-	/// </summary>
-	public float DriverStartSpacing = 2.5f;
+    /// <summary>
+    /// Radius within which the player has arrived at the waypoint.
+    /// </summary>
+    public float WaypointArriveRadius;
 
 
 
-	/// <summary>
-	/// The normal max speed of the AI drivers, before random adjustment.
-	/// </summary>
-	public float NormalMaxSpeed = 0.1f;
+    /// <summary>
+    /// The initial spacing of the drivers for the start.
+    /// </summary>
+    public float DriverStartSpacing = 2.5f;
 
 
 
-	/// <summary>
-	/// The max speed random adjustment range to apply.
-	/// </summary>
-	public float MaxSpeedAdjustmentRange = 0.015f;
+    /// <summary>
+    /// The normal max speed of the AI drivers, before random adjustment.
+    /// </summary>
+    public float NormalMaxSpeed = 0.1f;
+
+
+
+    /// <summary>
+    /// The max speed random adjustment range to apply.
+    /// </summary>
+    public float MaxSpeedAdjustmentRange = 0.015f;
 
 
 
@@ -107,84 +107,99 @@ public class GameController : MonoBehaviour
 
 
 
-	/// <summary>
-	/// Flag to control extra debugging info.
-	/// </summary>
-	public bool Debugging;
+    /// <summary>
+    /// Flag to control extra debugging info.
+    /// </summary>
+    public bool Debugging;
 
-	private bool isPaused;
+    private bool isPaused;
 
-	#endregion
-
-
-
-	#region Unity Methods
-
-	/// <summary>
-	/// Use this for initialization
-	/// </summary>
-	void Start ()
-	{
-		// Get the waypoints from the game objects around the track
-		Waypoint[] WaypointsFromTrack = Track.GetComponentsInChildren<Waypoint> ();
-		foreach (Waypoint CurrentWaypoint in WaypointsFromTrack) {
-			// Add the waypoint to the list
-			Waypoints.Add (new Vector2 (CurrentWaypoint.GetPosition().x, CurrentWaypoint.GetPosition().y));
-
-			if (Debugging) {
-				Instantiate (WaypointPrefab, CurrentWaypoint.GetPosition(), Quaternion.identity);
-			}
-		}
+    #endregion
 
 
-		// Create the driver game objects
-		AIDrivers = new List<GameObject> ();
-		for (int PlayerNum = 0; PlayerNum < NumberOfAIDrivers; PlayerNum++) {
-			// Calculate the initial position
-			Vector2 StartPosition = new Vector2 ((PlayerNum + 1) * DriverStartSpacing, 0);
 
-			// Create the driver in the UI environment
-			GameObject NewDriver = Instantiate (PlayerPrefab, StartPosition, Quaternion.identity);
-			AIDrivers.Add (NewDriver);
+    #region Unity Methods
 
-			// Create a new driver AI
-			string DriverName = "Player " + PlayerNum.ToString ();
-			NewDriver.GetComponent<AIPlayerController> ().Name = DriverName;
-			NewDriver.GetComponent<AIPlayerController> ().ArriveRadius = WaypointArriveRadius;
-			NewDriver.GetComponent<AIPlayerController> ().Waypoints = new List<Vector2> (Waypoints);
-			NewDriver.GetComponent<AIPlayerController> ().rand = rand;
-			NewDriver.GetComponent<AIPlayerController> ().Debugging = Debugging;
-			NewDriver.GetComponent<AIPlayerController> ().MaxSpeed = NormalMaxSpeed;
+    /// <summary>
+    /// Use this for initialization
+    /// </summary>
+    void Start()
+    {
+        // Get the waypoints from the game objects around the track
+        Waypoint[] WaypointsFromTrack = Track.GetComponentsInChildren<Waypoint> ();
+        foreach( Waypoint CurrentWaypoint in WaypointsFromTrack )
+        {
+            // Add the waypoint to the list
+            Waypoints.Add( new Vector2( CurrentWaypoint.GetPosition().x, CurrentWaypoint.GetPosition().y ) );
+
+            if( Debugging )
+            {
+                Instantiate( WaypointPrefab, CurrentWaypoint.GetPosition(), Quaternion.identity );
+            }
+        }
+
+
+        // Create the driver game objects
+        AIDrivers = new List<GameObject>();
+        for( int PlayerNum = 0; PlayerNum < NumberOfAIDrivers; PlayerNum++ )
+        {
+            // Calculate the initial position
+            Vector2 StartPosition = new Vector2 (Mathf.Floor(((PlayerNum+1)/2)) * DriverStartSpacing, 0);
+
+            // Stagger the drivers
+            if( PlayerNum % 2 == 0 )
+            {
+                StartPosition.y += -0.5f;
+            }
+            else
+            {
+                StartPosition.y += 0.5f;
+            }
+
+
+            // Create the driver in the UI environment
+            GameObject NewDriver = Instantiate( PlayerPrefab, StartPosition, Quaternion.Euler(0,0,90) );
+            AIDrivers.Add( NewDriver );
+
+            // Create a new driver AI
+            string DriverName = "Player " + PlayerNum.ToString ();
+            NewDriver.GetComponent<AIPlayerController>().Name = DriverName;
+            NewDriver.GetComponent<AIPlayerController>().ArriveRadius = WaypointArriveRadius;
+            NewDriver.GetComponent<AIPlayerController>().Waypoints = new List<Vector2>( Waypoints );
+            NewDriver.GetComponent<AIPlayerController>().rand = rand;
+            NewDriver.GetComponent<AIPlayerController>().Debugging = Debugging;
+            NewDriver.GetComponent<AIPlayerController>().MaxSpeed = NormalMaxSpeed;
+            NewDriver.GetComponent<AIPlayerController>().StuckSpeed = StuckSpeed;
             NewDriver.GetComponent<AIPlayerController>().StuckTimeout = StuckTimeout;
             NewDriver.GetComponent<AIPlayerController>().GetUnstuckTime = GetUnstuckTime;
-            NewDriver.GetComponent<SpriteRenderer> ().material.SetColor ("_Color", Random.ColorHSV (0f, 1f, 1f, 1f, 0.5f, 1f));
+            NewDriver.GetComponent<SpriteRenderer>().material.SetColor( "_Color", Random.ColorHSV( 0f, 1f, 1f, 1f, 0.5f, 1f ) );
 
-			// Get a random speed adjustment
-			double SpeedAdjustment = (rand.NextDouble () - 0.5) * MaxSpeedAdjustmentRange;
-			NewDriver.GetComponent<AIPlayerController> ().MaxSpeed += (float)SpeedAdjustment;
+            // Get a random speed adjustment
+            double SpeedAdjustment = (rand.NextDouble () - 0.5) * MaxSpeedAdjustmentRange;
+            NewDriver.GetComponent<AIPlayerController>().MaxSpeed += (float)SpeedAdjustment;
 
-			// Activate the driver navigaction
-			NewDriver.GetComponent<AIPlayerController> ().NavigationActive = true;
-		}
-		isPaused = false;
-	}
+            // Activate the driver navigation
+            NewDriver.GetComponent<AIPlayerController>().NavigationActive = true;
+        }
+        isPaused = false;
+    }
 
 
 
-	/// <summary>
-	/// Update is called once per frame
-	/// </summary>
-	void Update ()
-	{
-		// Check for esc pressed
-//		if (Input.GetKeyDown (KeyCode.Escape) && !isPaused) {
-//			for (int PlayerNum = 0; PlayerNum < NumberOfAIDrivers; PlayerNum++)
-//				AIDrivers [PlayerNum].GetComponent<AIPlayerController> ().NavigationActive = false;
-//		} else if (Input.GetKeyDown (KeyCode.Escape) && isPaused) {
-//			for (int PlayerNum = 0; PlayerNum < NumberOfAIDrivers; PlayerNum++)
-//				AIDrivers [PlayerNum].GetComponent<AIPlayerController> ().NavigationActive = true;
-//		}
-	}
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update()
+    {
+        // Check for esc pressed
+        //		if (Input.GetKeyDown (KeyCode.Escape) && !isPaused) {
+        //			for (int PlayerNum = 0; PlayerNum < NumberOfAIDrivers; PlayerNum++)
+        //				AIDrivers [PlayerNum].GetComponent<AIPlayerController> ().NavigationActive = false;
+        //		} else if (Input.GetKeyDown (KeyCode.Escape) && isPaused) {
+        //			for (int PlayerNum = 0; PlayerNum < NumberOfAIDrivers; PlayerNum++)
+        //				AIDrivers [PlayerNum].GetComponent<AIPlayerController> ().NavigationActive = true;
+        //		}
+    }
 
-	#endregion
+    #endregion
 }
